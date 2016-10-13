bf1b010e43478355c2f2b3469c37652c618916db
Yagiz Nizipli <yagiznizipli@me.com>
2016-10-13 22:48:41 +0300

    Use actual sourcebot version while creating new projects

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 const Structure = require('./structure');
 
+const request = require('request-promise');
 const Promise = require('bluebird');
 const app = require('commander');
 const VERSION = require('../package.json').version;
@@ -12,6 +13,14 @@ process.title = 'sourcebot';
 app
   .version(VERSION);
 
+  request({
+    url: 'http://registry.npmjs.org/sourcebot',
+    json: true
+  })
+  .then((registry) => {
+    console.log('reg', registry['dist-tags'].latest)
+  })
+
 app
   .command('create [path]')
   .alias('c')
@@ -20,29 +29,37 @@ app
   .action((name, options) => {
     const sourcebot = new Structure(name);
 
-    sourcebot
-      .createFolder()
-      .then(() => {
-        const packageJson = {
-          name: name,
-          version: '1.0.0',
-          main: 'index.js',
-          scripts: {
-            test: 'echo \"Error: no test specified\" && exit 1'
-          },
-          dependencies: {
-            sourcebot: '*'
-          }
-        };
+    request({
+      url: 'http://registry.npmjs.org/sourcebot',
+      json: true
+    })
+    .then((registry) => {
+      const sourcebotVersion = registry['dist-tags'].latest;
 
-        return Promise
-          .all([
-            sourcebot.createFile('package.json', JSON.stringify(packageJson, null, 2)),
-            sourcebot.createFile('index.js', require('./templates')),
-            sourcebot.createFile('.gitignore', 'node_modules/')
-          ]);
-      })
-      .catch(err => console.error(`Sourcebot: ${err.message}`))
+      return sourcebot
+        .createFolder()
+        .then(() => {
+          const packageJson = {
+            name: name,
+            version: '1.0.0',
+            main: 'index.js',
+            scripts: {
+              test: 'echo \"Error: no test specified\" && exit 1'
+            },
+            dependencies: {
+              sourcebot: `^${sourcebotVersion}`
+            }
+          };
+
+          return Promise
+            .all([
+              sourcebot.createFile('package.json', JSON.stringify(packageJson, null, 2)),
+              sourcebot.createFile('index.js', require('./templates')),
+              sourcebot.createFile('.gitignore', 'node_modules/')
+            ]);
+        })
+    })
+    .catch(err => console.error(`Sourcebot: ${err.message}`))
   });
 
 app.parse(process.argv);
